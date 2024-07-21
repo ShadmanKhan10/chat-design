0f05de94f3415def561a6337156777af4f678378
SHADMAN KHAN <khanshadman7777@gmail.com>
2024-07-21 16:39:15 +0530

    Aligned the chats

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Chat-Design</h1>
+      <div className="chats-container">
+        <div className="senders-chat">
+          <p>Hii How are you</p>
+        </div>
+        <div className="recievers-chat">
+          <p>I am fine</p>
+        </div>
+      </div>
     </>
   );
 }
