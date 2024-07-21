4fe8eccbcb119fb9e1c38401a87105fd991da191
SHADMAN KHAN <khanshadman7777@gmail.com>
2024-07-21 17:43:11 +0530

    Almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [screen, setScreen] = useState("sender");
   return (
     <>
       <div className="chats-container">
-        <div className="chat-box-container">
-          <div className="senders-chat">
-            <p className="msg-text">Hii How are you</p>
+        {screen === "sender" ? (
+          <div className="chat-box-container">
+            <div className="senders-chat">
+              <p className="msg-text">Hii How are you</p>
+            </div>
+            <div className="recievers-chat">
+              <p className="msg-text">I am fine</p>
+            </div>
           </div>
-          <div className="recievers-chat">
-            <p className="msg-text">I am fine</p>
+        ) : (
+          <div className="chat-box-container">
+            <div className="recievers-chat">
+              <p className="msg-text">I am fine</p>
+            </div>
+            <div className="senders-chat">
+              <p className="msg-text">Hii How are you</p>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </>
   );
