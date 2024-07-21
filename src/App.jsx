76f63d4326ff228cce66dabeccec90afe3242a04
SHADMAN KHAN <khanshadman7777@gmail.com>
2024-07-21 17:20:40 +0530

    completed basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ function App() {
   return (
     <>
       <div className="chats-container">
-        <div className="senders-chat">
-          <p>Hii How are you</p>
-        </div>
-        <div className="recievers-chat">
-          <p>I am fine</p>
+        <div className="chat-box-container">
+          <div className="senders-chat">
+            <p className="msg-text">Hii How are you</p>
+          </div>
+          <div className="recievers-chat">
+            <p className="msg-text">I am fine</p>
+          </div>
         </div>
       </div>
     </>
